b0a910567d0329165423d22090d9beb491293a67
bdasuncion <bry_of_the_sky@yahoo.com>
2023-10-06 15:21:33 +0800

    anonymous

--- pokedex/src/PokemonSummaryDataView.js
+++ pokedex/src/PokemonSummaryDataView.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from "react"
 
 export class PokemonSummaryDataView extends React.Component {
@@ -36,4 +37,44 @@ export class PokemonSummaryDataView extends React.Component {
 
         return returnString + rawId 
     }
+=======
+import React from "react"
+
+export class PokemonSummaryDataView extends React.Component {
+    render() {
+        const id = this.convertId(this.props.pid)
+        var image
+        try {
+            image = require("./images/" + id + ".png")
+        } catch {
+            return (<span></span>)
+        }
+
+        return (
+            <span>
+                <div>
+                    <img src={ image } alt={this.props.pname} width="64" height="64"/>
+                </div>
+                <div>
+                    Name: {this.props.pname}
+                </div>
+                <div>
+                    Type: {this.props.ptype}
+                </div>
+            </span>
+        )
+    }
+
+    convertId(id) {
+        const maxLengthString = 3
+        var rawId = id.toString()
+        var returnString = ""
+        const PAD = maxLengthString - rawId.length
+        for (var i = 0; i < PAD; ++i) {
+            returnString += "0"
+        }
+
+        return returnString + rawId 
+    }
+>>>>>>> Stashed changes
 }
